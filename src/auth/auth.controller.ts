31f6a13d699be47619186ef64f37203ff67286fd
Juan Barreto <juan2.manuelbarreto@gmail.com>
2024-10-09 00:52:57 -0400

    chore: implemented JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -12,7 +22,13 @@ export class AuthController {
   }
 
   @Post('signup')
-    signUp(@Body() signUpDto: Record<string, any>) {
-        return this.authService.signUp(signUpDto.username, signUpDto.password);
-    }
+  signUp(@Body() signUpDto: Record<string, any>) {
+    return this.authService.signUp(signUpDto.username, signUpDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('me')
+  me(@Request() req) {
+    return req.user;
+  }
 }
